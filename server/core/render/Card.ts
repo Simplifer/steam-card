37561ea87a4fdd5d8a729bfa1b4a1d9164317ee9
Simplifer <820317041@qq.com>
2023-07-07 09:19:21 +0800

    chore: default settings

--- server/core/render/Card.ts
+++ server/core/render/Card.ts
@@ -217,7 +217,7 @@ class Card {
             <img class="avatar" src="${this.avatarUrlBase64}" width="60" height="60" />
             <div class="status">
               <div style="font-size:12px;font-weight:bold">
-                  ${this.name} | ${this.accountYear}${this.i18n.get('years')}
+                  ${this.name} | ${this.accountYear}${this.i18n.get('account-year')}
               </div>
               <div style="font-size:12px;font-weight:bold">
                   LV. ${this.playerLevel}
